4e61964578b435610fbcd76435113cf392e60779
Robert Dodier <robert_dodier@users.sourceforge.net>
2018-03-14 14:12:15 -0700

    Remove example project foo, since there exists maxima-foo as a separate repo on Github.

--- foo/src/bar1.lisp
+++
@@ -1,3 +0,0 @@
-(format t "HELLO FROM BAR1.LISP; *DEFAULT-PATHNAME-DEFAULTS*=~S~%" *default-pathname-defaults*)
-(defun maxima::$mybar1 (x) (maxima::m* x x))
-(defun maxima::$mybar2 () asdf::foo-toplevel)
