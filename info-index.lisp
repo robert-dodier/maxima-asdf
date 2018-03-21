e0d26ee7c5ab7f773eaf83a918c61e0de65a504b
Robert Dodier <robert_dodier@users.sourceforge.net>
2018-03-21 11:31:24 -0700

    Cut out methods which were only punting via CALL-NEXT-METHOD.

--- info-index.lisp
+++ info-index.lisp
@@ -10,12 +10,6 @@
 (defclass info-index (cl-source-file)
   ((type :initform "lisp"))) ;; ISN'T THIS INHERITED FROM CL-SOURCE-FILE ??
 
-(defmethod perform ((o load-source-op) (c info-index))
-  (call-next-method))
-
-(defmethod perform ((o load-op) (c info-index))
-  (call-next-method))
-
 (defmethod input-files ((o compile-op) (c info-index))
   (let*
     ((foo (call-next-method))
@@ -24,9 +18,6 @@
      (bar "lisp")
      (baz (merge-pathnames (make-pathname :type bar) (first foo))))
     (list baz)))
-
-(defmethod output-files ((o compile-op) (c info-index))
-  (call-next-method))
 
 (defmethod perform ((o compile-op) (c info-index))
   (let*
